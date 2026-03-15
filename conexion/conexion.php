ad72838bbb619e541b73c800a07c75658566a4a2
HansRubio2023 <126277906+HansRubio2023@users.noreply.github.com>
2026-03-15 17:35:31 -0300

    conexion exitosa a la base de datos

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,65 +1,38 @@
 <?php
-header('Content-Type: application/json');
+
+// Creamos una función de conexión a la base de datos
+function connection()
+{
+    //Creamos las variables de conexión y los parametros
+    $host="127.0.0.1";
+    $user="root";
+    $pass="";
+
+    // Vamos a definir una variable para la base de datos
+    $bd="clinica_psicologica";
+
+    //Definir una variable para la conexión a la base de datos
+    //Definir el método de conexión mysqli_connect()
+    $connect = mysqli_connect($host, $user, $pass);
+
+    //Seleccionar la base de datos y la conexión
+    mysqli_select_db($connect, $bd);
+    
+    //Vamos a retornar la conexión de la variable connect 
+    return $connect;   
+};
 
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
-
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "clinica_psicologica";
-
-try {
- 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-   
-    if ($conn->connect_error) {
-        throw new Exception("Conexión fallida: " . $conn->connect_error);
-    }
-
-   
-    $username = $_POST['user'];
-    $password = $_POST['pass'];
-
-   
-    $stmt = $conn->prepare("SELECT p.contrasena_usuario, p.id_perfil 
-                            FROM perfiles p 
-                            WHERE p.nombre_usuario = ?");
-    $stmt->bind_param("s", $username);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        if (password_verify($password, $row['contrasena_usuario'])) {
-            switch ($row['id_perfil']) {
-                case 1:
-                    $role = 'cliente';
-                    break;
-                case 2:
-                    $role = 'empleado';
-                    break;
-                case 3:
-                    $role = 'admin';
-                    break;
-                default:
-                    $role = 'desconocido';
-            }
-            echo json_encode(["success" => true, "role" => $role]);
-        } else {
-            echo json_encode(["success" => false, "message" => "Contraseña incorrecta"]);
-        }
-    } else {
-        echo json_encode(["success" => false, "message" => "Usuario no encontrado"]);
-    }
-
-    $stmt->close();
-    $conn->close();
-
-} catch (Exception $e) {
-    echo json_encode(["success" => false, "message" => "Error en el servidor: " . $e->getMessage()]);
+$mi_conexion = connection();
+if(!$mi_conexion)
+{
+   //Mostrar un mensaje de conexión no existosa
+   echo "Conexión no es existosa: " 
+   . mysqli_connect_error();
+    return null;
 }
+
+echo "<h3>Mostrando resultado de la conexión:</h3> 
+<h2>Conexión exitosa</h2>";
+
 ?>
